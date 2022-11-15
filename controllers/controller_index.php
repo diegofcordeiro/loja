be1c4ab3352783bb2209dfb7e457d945049f3455
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2022-11-15 23:10:06 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -8571,7 +8571,6 @@ class index extends controller {
 			$bill = $this->pay_bill_vindi($id_client,$payment_met,$value);
 			echo 'aqui';
 			print_r($bill['id']);
-			print_r($bill->id);
 			$id_charge = $bill->charges[0]->id;
 			$id_trans = $bill->id;
 			echo "<br>";

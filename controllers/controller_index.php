fcea7157cf98adfad673411219bb43f0ce787f05
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2022-11-15 23:15:33 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -8570,7 +8570,8 @@ class index extends controller {
 
 			$bill = $this->pay_bill_vindi($id_client,$payment_met,$value);
 			echo 'aqui';
-			echo($bill->id);
+			$bill = json_decode($bill);
+			print_r($bill);exit;
 			$id_charge = $bill->charges[0]->id;
 			$id_trans = $bill->id;
 			echo "<br>";

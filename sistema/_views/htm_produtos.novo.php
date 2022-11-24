3c776e9dce86d7b78704644e9f4148c541da748d
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2022-11-24 16:21:42 +0000

    Update htm_produtos.novo.php

--- sistema/_views/htm_produtos.novo.php
+++ sistema/_views/htm_produtos.novo.php
@@ -11,7 +11,7 @@
 			<div class="col-md-12">
 				<select class="form-control select2" id="ref" name="ref">
 					<?php foreach($lista_trilha_lms as $trilha){ ?>
-						<option data-title="<?=$trilha['nome_trilha']?>" value='<?=$trilha['id_trilha']?>'><?=$trilha['nome_trilha']?></option>
+						<option data-title="<?=$trilha['nome_trilha']?>" value='<?=$trilha['id_trilha']?>'><?=$trilha['id_trilha'].' - '.$trilha['nome_trilha']?></option>
 					<?php }?>
 				</select>
 				<input name="titulo" type="hidden" id="titulo" class="form-control" value="<?=$data->titulo?>" >

fd8e23302e5e7b81ab9d04c2ab404b4d3d3b00e4
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2022-11-29 17:07:02 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -8623,7 +8623,7 @@ echo $data_carrinho->produto_ref;
 		
 		$value_sub = '';
 		foreach($recorrentes as $key => $recorrencia){
-			echo '<pre>';print_r($recorrentes);exit;
+			echo '<pre>';print_r($recorrencia);exit;
 
 			$point_sub = (count($recorrentes)>1 && $key == 0) ? ',' : '';
 			$value_sub .= '{"plan_id": "'.$recorrencia->produto_assinatura.'","customer_id": "'.$id_client.'","payment_method_code": "'.$payment_met.'","product_items": [{"product_id": "1040228"}]}'.$point_sub.'';

7ae6f3735f790190621c7ef865727f694a3833f0
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2022-11-29 13:24:18 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -17,7 +17,7 @@ class index extends controller {
 		$dados['_sessao'] = $this->_sessao;
 		$dados['_acesso'] = $this->_acesso;
 		$dados['_nome_usuario'] = $this->_nome_usuario;
-echo'<pre>';print_r('asasdasd');exit;	
+	
 		// lista_produto_comprado
 		if($dados['_nome_usuario'] != 'Visitante'){ 
 			$pedidos = new model_pedidos();
@@ -701,7 +701,7 @@ echo'<pre>';print_r('asasdasd');exit;
 				$new_melhor_qualificado[$nm_canal_melhor_qualificado] = array($obj_melhor_qualificado);
 			}
 		}
-
+echo'<pre>';print_r('asasdasd');exit;
 		$dados['cat_selecionada'] = ($_POST['categoria'] > 0 ? $_POST['categoria'] : 0);
 		$dados['autor_selecionado'] = ($_POST['autor'] > 0 ? $_POST['autor'] : 0);
 		$dados['buscar_campo'] = $_POST['buscar1'];

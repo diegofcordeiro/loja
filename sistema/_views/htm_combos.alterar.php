da6b6c82c12c26422c5798e17a4286b38dd005a5
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-10 22:09:22 +0000

    Update htm_combos.alterar.php

--- sistema/_views/htm_combos.alterar.php
+++ sistema/_views/htm_combos.alterar.php
@@ -155,7 +155,7 @@
                                 <div class="form-group">
                                     <label class="col-md-12">Link para checkout</label>
                                     <div class="col-md-12">
-                                       <p><?=URL?>/combo/<?=$data->id?></p>
+                                       <!-- <p><?=URL?>index/combo/<?=$data->id?></p> -->
                                     </div>
                                 </div>
                             </div>

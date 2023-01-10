871cdc09eb055726cccb80ba28ea5732e3b90b78
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-10 00:40:46 +0000

    Update htm_produtos.detalhes.php

--- views/htm_produtos.detalhes.php
+++ views/htm_produtos.detalhes.php
@@ -769,7 +769,7 @@
 						<div class='col-xs-12 col-sm-5 col-md-5'>
 							<img src="<?=DOMINIO?>/arquivos/banner_img_produto_g/<?=$data->codigo?>/<?=$data->banner?>" alt="">
 						</div>
-						<?php if($only_combo == 1){?>
+						<?php if($data->$only_combo == 1){?>
 							<div class='col-xs-12 col-sm-7 col-md-7'>
 						<?php }else{?>
 							<div class='col-xs-12 col-sm-7 col-md-7'>
@@ -778,8 +778,8 @@
 								<div class="style1" style="margin-top: 20px;"><?=$data->summary?></div>
 							</div>
 						</div>
-						<?php if($only_combo !== 1){?>
-							<div class='col-xs-12 col-sm-3 col-md-3' id="<?=$only_combo?>">
+						<?php if($data->$only_combo == 0){?>
+							<div class='col-xs-12 col-sm-3 col-md-3' >
 								<div class="produto_detalhes_valor" style="text-align: center;">
 									<div class="produtos_detalhes_valortotal" style="text-decoration: line-through;"></div>
 									<div class="produtos_detalhes_valortotal" style="margin-top: 0px; font-weight:500;color:#2C3E50">Investimento:</div>
@@ -803,7 +803,7 @@
 								<div class="style1" style="margin-top: 20px;"><?=$data->summary?></div>
 							</div>
 						</div>
-						<?php if($only_combo !== 1){?>
+						<?php if($data->$only_combo == 0){?>
 							<div class='col-xs-12 col-sm-6 col-md-6' id="<?=$only_combo?>">
 								<div class="produto_detalhes_valor" style="text-align: center;">
 									<div class="produtos_detalhes_valortotal" style="text-decoration: line-through;"></div>

b3de9757efb7fbd5dae73f4da2bc63235af31a5c
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-10 11:39:20 +0000

    Update htm_combos.alterar.php

--- sistema/_views/htm_combos.alterar.php
+++ sistema/_views/htm_combos.alterar.php
@@ -64,6 +64,12 @@
   <link rel="stylesheet" href="<?=LAYOUT?>plugins/colorpicker/bootstrap-colorpicker.min.css">
   <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
   <link rel="stylesheet" href="<?=LAYOUT?>docsupport/style.css">
   <link rel="stylesheet" href="<?=LAYOUT?>docsupport/prism.css">
   <link rel="stylesheet" href="<?=LAYOUT?>chosen.css">
@@ -231,6 +237,7 @@
                               <div class="col-md-12">
                                 <!-- <input name="usar_desconto" type="text" class="form-control" value="<?=$data->usar_desconto?>" > -->
                                 <input type="checkbox" class="custom-control-input" name="usar_desc" id="customSwitches">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch1">
                               </div>
                             </div>
                           </div>

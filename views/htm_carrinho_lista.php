678c03ef2778655a94a16e47f63f7725777607f0
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-10 15:12:27 +0000

    Update htm_carrinho_lista.php

--- views/htm_carrinho_lista.php
+++ views/htm_carrinho_lista.php
@@ -129,18 +129,8 @@
 
 			$n++;
 		}
-		$subtotal_ = '';
-		foreach ($values as $key2 => $value) {
-			if($value['usar_valor_vindi'] == 1){
-				$valor_unitario = '-';
-				$total_geral = '-';
-				$subtotal_ = $value['valor_total_combo_vindi'];
-			}else{
-				$valor_unitario = "R$ ".$value['total_unitario'];
-				$total_geral = "R$ ".$value['total_quantidade'];
-				$subtotal_ = $value['produto_valor'];
-			}
-		}
+
+		echo '<pre>';print_r($values);exit;
 
 		echo "
 			<tr>

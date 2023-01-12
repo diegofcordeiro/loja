6c868eb017a6ae6e281913f70ffe1b287a803b42
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-12 16:31:35 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -9048,7 +9048,7 @@ class index extends controller {
 				
 			}
 		}	
-		echo '<pre>';print_r($recorrentes);exit;
+		
 		/////////     RECCORENTE    /////////////
 		foreach($recorrentes as $key => $recorrencia){
 			$amout = 0;

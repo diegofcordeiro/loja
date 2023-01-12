94b61d838fb885f227e6ad2b08c2efda9db6d1e6
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-12 14:58:17 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -7497,7 +7497,7 @@ class index extends controller {
 					$data_produto = $coisas_produto->fetch_object();
 
 				}
-				print_r($data_carrinho);exit;
+				echo'<pre>';print_r($data_carrinho);exit;
 				$total_unitario = $data_carrinho->valor_total;
 				$total_quantidade = $valores->trata_valor_calculo($total_unitario * $data_carrinho->quantidade);
 				$valor_subtotal = $valores->trata_valor_calculo($valor_subtotal + $total_quantidade);

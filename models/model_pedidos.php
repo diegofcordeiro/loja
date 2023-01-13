110fc24298e07a95d06ade9cbc24c496eb27d227
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-13 22:07:52 +0000

    Update model_pedidos.php

--- models/model_pedidos.php
+++ models/model_pedidos.php
@@ -60,7 +60,7 @@ Class model_pedidos extends model{
 				$new_lista[$sessao] = array($obj_lista);
 			}
 		}
-
+echo '<pre>';print_r($new_lista);exit;
 		$final_lista_full = array();
 		foreach ($new_lista as $key => $obj_list) {
 			$final_lista = array();
@@ -76,7 +76,7 @@ Class model_pedidos extends model{
 			$final_lista_full[$key] = $final_lista;
 			
 		}	
-echo '<pre>';print_r($final_lista);exit;
+
 		return $final_lista;
 	}	
 

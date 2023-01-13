37fcfac389020cd871618786d4edfab6508d8543
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-13 19:29:18 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -8741,7 +8741,6 @@ class index extends controller {
 
 				$sql2 = "SELECT * FROM curso WHERE id_trilha = '$data_carrinho->produto_ref' ";
 				if ($result2 = $mysqli->query($sql2)) {
-				print_r($result2);exit;
 					while ($obj2 = $result2->fetch_object()) {
 						$array = array(
 							'id_usuario' => $id_usuario, 

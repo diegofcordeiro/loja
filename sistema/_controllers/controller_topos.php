06c6c66823de93040d0c4a8e9f4b47a01aa99c55
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-13 16:03:08 +0000

    Update controller_topos.php

--- sistema/_controllers/controller_topos.php
+++ sistema/_controllers/controller_topos.php
@@ -930,7 +930,7 @@ class topos extends controller {
 				$db = new mysql();
 				$db->alterar("adm_config", array(
 					"logo"=>$nome_arquivo
-				), " id=4 ");
+				), " id=1 ");
 
 				$this->irpara(DOMINIO.$this->_controller.'/alterar/codigo/'.$codigo.'/aba/logo_admin');
 

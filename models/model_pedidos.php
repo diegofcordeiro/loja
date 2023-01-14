d28735cd657deb77a20e24bb991947ad3b259638
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-14 11:02:25 +0000

    Update model_pedidos.php

--- models/model_pedidos.php
+++ models/model_pedidos.php
@@ -125,7 +125,7 @@ Class model_pedidos extends model{
 				$new_lista[$id_combo] = array($obj_lista);
 			}
 		}		
-		// echo '<pre>';print_r($new_lista);exit;
+		echo '<pre>';print_r($lista);exit;
 		
  		
  		return $lista;

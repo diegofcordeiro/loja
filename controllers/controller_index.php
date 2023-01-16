6854fdf146a308bd318599d5df48ed8b48a75a8b
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-16 17:11:02 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -9063,8 +9063,8 @@ class index extends controller {
 
 				if($bill['bill']['charges'][0]['status'] == 'paid'){ 
 					$status = 4;
+						print_r($recorrencia->produto_ref.'-'.$cod.'-'.$cpf);
 						$this->integrar_trilha_lms($recorrencia->produto_ref,$cod, $cpf);
-					
 				}else{
 					$status = 1;
 				}

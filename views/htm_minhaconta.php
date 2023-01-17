6bcdf1e4f1059263b64c5d8be7efa1346291d32a
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-17 21:03:30 +0000

    Update htm_minhaconta.php

--- views/htm_minhaconta.php
+++ views/htm_minhaconta.php
@@ -904,8 +904,8 @@
 												print_r($seven_days);
 												echo ' compra :<br>';
 												print_r($data_compr);
-												echo ' Original :<br>';
-												
+												echo '<br>';
+												print_r($data_compr < $seven_days);
 												if($data_compr < $seven_days){
 													$est = "<a href='$estorno' style='color:blue;'>Estornar</a>";
 												}

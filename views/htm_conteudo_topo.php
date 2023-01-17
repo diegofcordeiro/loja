70386d854155dc827cd151429fb7950738d1e4e2
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-17 23:25:57 +0000

    Update htm_conteudo_topo.php

--- views/htm_conteudo_topo.php
+++ views/htm_conteudo_topo.php
@@ -82,171 +82,171 @@ $url = $_GET['url'];
 	}
 	?>
 </style>
-	<style type="text/css">
-		a.botao_padrao{
+<style type="text/css">
+	a.botao_padrao{
+	background-color: <?=$primaria?> !important;
+	}
+	/* .botao_padrao:hover {
 		background-color: <?=$primaria?> !important;
-		}
-		/* .botao_padrao:hover {
-			background-color: <?=$primaria?> !important;
-			color: #fff !important;
-		} */
-		.botao_padrao{
-			background-color: <?=$primaria?> !important;
-			border: none;
-    		color: white;
-		}
+		color: #fff !important;
+	} */
+	.botao_padrao{
+		background-color: <?=$primaria?> !important;
+		border: none;
+		color: white;
+	}
 
-		.points, .yellow_points{
-			color:<?=$secundaria?>
-		}
-		.laranja_points{
-			background: <?=$secundaria?> !important;
-			padding: 4px 8px;
-			font-size: 10px;
-			border-radius: 4px;
-			margin-left: 6px;
-		}
-		.estrela_amarela {
-			color: <?=$secundaria?>;
-		}
-		.item button:hover, .cart:hover {
-			background: <?=$secundaria?> !important;
-		}
-		@media (max-width: 770px){
-			.logo_div {
-				margin: 0 auto;
-			}
-			.li_loja a {
-				padding: 10px 10px !important;
-			}
-			#header{
-				position: relative !important;
-			}
-			.margemtopo{
-				display: none !important;
-			}
-		}
-		a.logo {
-			display: inline-block;
-			width: 100%;
-			margin: 0px;
-			margin-bottom: 15px;
-		}
-		.ul_loja {
-			list-style-type: none;
-			margin: 0;
-			padding: 0;
-			overflow: hidden;
-		}
-		.li_loja {
-			float: left;
-			padding-bottom: 0px;
+	.points, .yellow_points{
+		color:<?=$secundaria?>
+	}
+	.laranja_points{
+		background: <?=$secundaria?> !important;
+		padding: 4px 8px;
+		font-size: 10px;
+		border-radius: 4px;
+		margin-left: 6px;
+	}
+	.estrela_amarela {
+		color: <?=$secundaria?>;
+	}
+	.item button:hover, .cart:hover {
+		background: <?=$secundaria?> !important;
+	}
+	@media (max-width: 770px){
+		.logo_div {
+			margin: 0 auto;
 		}
 		.li_loja a {
-			display: block;
-			color: #344456;
-			text-align: center;
-			padding: 30px 18px;
-			text-decoration: none;
+			padding: 10px 10px !important;
 		}
-		.li_loja a:hover {
-			background-color: <?=$primaria?>;
-			color:white;
+		#header{
+			position: relative !important;
 		}
-		a.logo {
-			display: inline-block;
-			width: 100%;
-			margin: 0px;
-			margin-top: 8px;
-			margin-bottom: 0px;
+		.margemtopo{
+			display: none !important;
 		}
-		/* .logo_div {
-			width: 100%;
-			padding: 10px 0px;
-		} */
-		.busca_div, .div_botoes_topo{
-			padding: 10px 0px;margin-top: 10px;margin-bottom: -1px;
-		}
-		.span_btn{padding: 5px 10px 5px 10px; border-radius: 5px;border: 1px #2c3e4f solid;}
-		.span_btn_cad{padding: 5px 10px 5px 10px;border-radius: 5px;border: 1px <?=$primaria?> solid;background: <?=$primaria?>;color: white !important;}
-		.busca_input {
-			border-top: none;
-			border-left: none;
-			border-bottom: none;
-			border-right: none;
-			height: 25px;
-			border-radius: 0px;
-			color: #575757 !important;
-			font-size: 14px;
-			width: 300px;
-			padding: 10px;
-			box-shadow: none;
-			background-color: #cccccc !important;
-		}
-		.busca_botao {
-			background-color: #f8f8f8 !important;
-			border-top: none;
-			border-right: none;
-			border-bottom: none;
-			height: 32px;
-			width: 0px;
-			border-radius: none;
-			background-repeat: no-repeat;
-			background-position: center;
-			color: #575757 !important;
-			font-size: 14px;
-		}
-		.busca_div {
-    		padding: 20px 0px !important;
-		}
-		.selected_orange{background: <?=$primaria?>;color: white !important;}
-		.selected_orange_text{color: white !important;}
+	}
+	a.logo {
+		display: inline-block;
+		width: 100%;
+		margin: 0px;
+		margin-bottom: 15px;
+	}
+	.ul_loja {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+	}
+	.li_loja {
+		float: left;
+		padding-bottom: 0px;
+	}
+	.li_loja a {
+		display: block;
+		color: #344456;
+		text-align: center;
+		padding: 30px 18px;
+		text-decoration: none;
+	}
+	.li_loja a:hover {
+		background-color: <?=$primaria?>;
+		color:white;
+	}
+	a.logo {
+		display: inline-block;
+		width: 100%;
+		margin: 0px;
+		margin-top: 8px;
+		margin-bottom: 0px;
+	}
+	/* .logo_div {
+		width: 100%;
+		padding: 10px 0px;
+	} */
+	.busca_div, .div_botoes_topo{
+		padding: 10px 0px;margin-top: 10px;margin-bottom: -1px;
+	}
+	.span_btn{padding: 5px 10px 5px 10px; border-radius: 5px;border: 1px #2c3e4f solid;}
+	.span_btn_cad{padding: 5px 10px 5px 10px;border-radius: 5px;border: 1px <?=$primaria?> solid;background: <?=$primaria?>;color: white !important;}
+	.busca_input {
+		border-top: none;
+		border-left: none;
+		border-bottom: none;
+		border-right: none;
+		height: 25px;
+		border-radius: 0px;
+		color: #575757 !important;
+		font-size: 14px;
+		width: 300px;
+		padding: 10px;
+		box-shadow: none;
+		background-color: #cccccc !important;
+	}
+	.busca_botao {
+		background-color: #f8f8f8 !important;
+		border-top: none;
+		border-right: none;
+		border-bottom: none;
+		height: 32px;
+		width: 0px;
+		border-radius: none;
+		background-repeat: no-repeat;
+		background-position: center;
+		color: #575757 !important;
+		font-size: 14px;
+	}
+	.busca_div {
+		padding: 20px 0px !important;
+	}
+	.selected_orange{background: <?=$primaria?>;color: white !important;}
+	.selected_orange_text{color: white !important;}
 
-		@media (max-width: 770px){
-			.desk_menu{display:none !important}
-			.mobile_menu{display:block !important}
-			.container-fluid>.navbar-collapse {margin-left: 0;}
+	@media (max-width: 770px){
+		.desk_menu{display:none !important}
+		.mobile_menu{display:block !important}
+		.container-fluid>.navbar-collapse {margin-left: 0;}
+	}
+	@media (min-width: 771px){
+		.mobile_menu{display:none !important}
+		.desk_menu{display:block !important}
+	}
+	@media (min-width: 1369px){
+		.logo_div {
+			width: 60%;
+			padding: 10px 0px;
 		}
-		@media (min-width: 771px){
-			.mobile_menu{display:none !important}
-			.desk_menu{display:block !important}
+	}
+	@media (min-width: 1101px) and (max-width: 1368px){
+		.logo_div {
+			width: 90%;
+			padding: 10px 0px;
 		}
-		@media (min-width: 1369px){
-			.logo_div {
-				width: 60%;
-				padding: 10px 0px;
-			}
+	}
+	@media (max-width: 1100px){
+		.logo_div {
+			width: 80%;
+			padding: 10px 0px;
 		}
-		@media (min-width: 1101px) and (max-width: 1368px){
-			.logo_div {
-				width: 90%;
-				padding: 10px 0px;
-			}
+		.logo_div_desk {
+			width: 100% !important;
+			padding: 15px 0px !important;
 		}
-		@media (max-width: 1100px){
-			.logo_div {
-				width: 80%;
-				padding: 10px 0px;
-			}
-			.logo_div_desk {
-				width: 100% !important;
-				padding: 15px 0px !important;
-			}
-		}
-		.mobile_menu{margin: 0 !important;padding: 0 !important;}
-		.logo_mobile_{float: left;max-width: 140px;margin-left: 5px;}
-		.navbar-default {border: none !important;margin-top: 15px !important;margin-bottom: -5px !important;}
-		.navbar-collapse.collapse {padding-top: 0px !important;border: none !important;}
-		.navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a:hover {
-			color: white;
-			background-color: <?=$primaria?>;
-		}
-		.container-fluid {padding-right: 0;}
-		.rodape_contatos span {line-height: 20px;}
-		.dropdown-menu li a:hover, .dropdown-menu li a:focus {
-    		background-color: <?=$primaria?> !important;
-		}
-	</style>
+	}
+	.mobile_menu{margin: 0 !important;padding: 0 !important;}
+	.logo_mobile_{float: left;max-width: 140px;margin-left: 5px;}
+	.navbar-default {border: none !important;margin-top: 15px !important;margin-bottom: -5px !important;}
+	.navbar-collapse.collapse {padding-top: 0px !important;border: none !important;}
+	.navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a:hover {
+		color: white;
+		background-color: <?=$primaria?>;
+	}
+	.container-fluid {padding-right: 0;}
+	.rodape_contatos span {line-height: 20px;}
+	.dropdown-menu li a:hover, .dropdown-menu li a:focus {
+		background-color: <?=$primaria?> !important;
+	}
+</style>
 	<header id="header">
 		<div class="topo6">
 			<div id="topo" class="header-middle">
@@ -265,8 +265,9 @@ $url = $_GET['url'];
 							<ul class="ul_loja">
 								<li class="li_loja <?=($url == '' ? 'selected_orange' : '')?>"><a <?=($url == '' ? 'class="selected_orange_text"' : '')?> href="<?=DOMINIO?>">Home</a></li>
 								<li class="li_loja <?=($url == 'index/canal' ? 'selected_orange' : '')?>"><a <?=($url == 'index/canal' ? 'class="selected_orange_text"' : '')?> href="<?=DOMINIO.$controller?>/canal">Canais</a></li>
-								<li class="li_loja"><a id="meus_cursos" href="">Meus Cursos</a></li>
-								
+								<?php if(!$_nome_usuario == 'Visitante'){ ?>
+									<li class="li_loja"><a id="meus_cursos" href="">Meus Cursos</a></li>
+								<?php } ?>
 							</ul>
 						</div>
 						<?php if($_nome_usuario == 'Visitante'){ ?>

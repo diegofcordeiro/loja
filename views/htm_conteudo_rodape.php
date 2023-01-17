ef31792ecf38f9fe6bd0e3d4b07d328bbf784677
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-17 17:26:46 +0000

    Update htm_conteudo_rodape.php

--- views/htm_conteudo_rodape.php
+++ views/htm_conteudo_rodape.php
@@ -246,7 +246,7 @@ $botao_aviso = $conteudo_sessao['botao_aviso'];
 		<script>
 		$("#meus_cursos").click(function(e){
 			e.preventDefault();
-			var Cp    = <?=base64_encode($_SESSION['usuario_cpf'])?>;
+			var Cp    = <?=$_SESSION['usuario_cpf']?>;
 			var code_ = <?=$_SESSION['usuario_cpf']?>;
 			var url_webapp = "<?=URL_BASE?>webapp/integra.php?token=<?=base64_encode($_SESSION['usuario_cpf'])?>";
 			window.location.replace(url_webapp);

40c4053fce1bb8ac3da32c975a3f3c31829fad47
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-20 16:39:15 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -12,10 +12,10 @@ class index extends controller {
 		ini_set('display_startup_errors', TRUE);
 
 		// require_once dirname(__FILE__) . '/../Classes/PHPExcel/IOFactory.php';
-		require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/Excel/PHPExcel/IOFactory.php';
+		require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/Excel/PHPExcel.php';
 		// print_r( $_SERVER['DOCUMENT_ROOT'].'/vendor/Excel/PHPExcel/IOFactory.php');
-		$objPHPExcel = PHPExcel_IOFactory::load("lists.xlsx");
-		// print_r($objPHPExcel);
+		$objPHPExcel = new PHPExcel_Reader_excel5();	
+		print_r($objPHPExcel);
 	}
 	public function inicial(){
 

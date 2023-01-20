3f9d3bf3c0118c322f55ee8ee40bee3a11262df8
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-20 17:03:22 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -13,42 +13,37 @@ class index extends controller {
 		echo '<pre>';
 		$dados = fopen('test.csv', 'r');
 		$linha = fgetcsv($dados,1000,',');
-		foreach ($linha as $key => $l){
-			echo '<pre>';
-			print_r($key);
-			echo '<br>';
-			print_r($l);
+
+		while($linha = fgetcsv($dados,1000,',')){
+			echo($linha[0]).'<br>';
+			echo($linha[1]).'<br>';
+			echo($linha[2]).'<br>';
+			echo($linha[3]).'<br>';
+			echo($linha[4]).'<br>';
+			echo($linha[5]).'<br>';
+			echo($linha[6]).'<br>';
+			echo($linha[7]).'<br>';
+			echo($linha[8]).'<br>';
+			echo($linha[9]).'<br>';
+			echo($linha[10]).'<br>';
+			echo($linha[11]).'<br>';
+			echo($linha[12]).'<br>';
+			echo($linha[13]).'<br>';
+			echo($linha[14]).'<br>';
+			echo($linha[15]).'<br>';
+			echo($linha[16]).'<br>';
+			echo($linha[17]).'<br>';
+			echo($linha[18]).'<br>';
+			echo($linha[19]).'<br>';
+			echo($linha[20]).'<br>';
+			echo($linha[21]).'<br>';
+			echo($linha[22]).'<br>';
+			echo($linha[23]).'<br>';
+			echo($linha[24]).'<br>';
+			echo($linha[25]).'<br>';
+			echo($linha[26]).'<br>';
+			echo '<hr>';
 		}
-		// while($linha = fgetcsv($dados,1000,',')){
-		// 	echo($linha[0]).'<br>';
-		// 	echo($linha[1]).'<br>';
-		// 	echo($linha[2]).'<br>';
-		// 	echo($linha[3]).'<br>';
-		// 	echo($linha[4]).'<br>';
-		// 	echo($linha[5]).'<br>';
-		// 	echo($linha[6]).'<br>';
-		// 	echo($linha[7]).'<br>';
-		// 	echo($linha[8]).'<br>';
-		// 	echo($linha[9]).'<br>';
-		// 	echo($linha[10]).'<br>';
-		// 	echo($linha[11]).'<br>';
-		// 	echo($linha[12]).'<br>';
-		// 	echo($linha[13]).'<br>';
-		// 	echo($linha[14]).'<br>';
-		// 	echo($linha[15]).'<br>';
-		// 	echo($linha[16]).'<br>';
-		// 	echo($linha[17]).'<br>';
-		// 	echo($linha[18]).'<br>';
-		// 	echo($linha[19]).'<br>';
-		// 	echo($linha[20]).'<br>';
-		// 	echo($linha[21]).'<br>';
-		// 	echo($linha[22]).'<br>';
-		// 	echo($linha[23]).'<br>';
-		// 	echo($linha[24]).'<br>';
-		// 	echo($linha[25]).'<br>';
-		// 	echo($linha[26]).'<br>';
-		// 	echo '<hr>';
-		// }
 
 
 	}

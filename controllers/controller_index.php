f46f385b3ba2067fa4099267da0a63477b851423
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-20 16:56:49 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -13,7 +13,15 @@ class index extends controller {
 		echo '<pre>';
 		$dados = fopen('test.csv', 'r');
 		while($linha = fgetcsv($dados,1000,',')){
-			print_r($linha);
+			echo($linha[0]).'<br>';
+			echo($linha[1]).'<br>';
+			echo($linha[2]).'<br>';
+			echo($linha[3]).'<br>';
+			echo($linha[4]).'<br>';
+			echo($linha[5]).'<br>';
+			echo($linha[6]).'<br>';
+			echo($linha[7]).'<br>';
+			echo '<hr>';
 		}
 
 

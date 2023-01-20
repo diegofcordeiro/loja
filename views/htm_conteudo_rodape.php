6c2b5600fd444087b7a847292faf48e79b656269
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-20 13:03:21 +0000

    Update htm_conteudo_rodape.php

--- views/htm_conteudo_rodape.php
+++ views/htm_conteudo_rodape.php
@@ -243,7 +243,11 @@ $botao_aviso = $conteudo_sessao['botao_aviso'];
 	<script type="text/javascript">function showIt2() {document.getElementById("msg1").style.visibility = "visible";}    setTimeout("showIt2()", 5000); function hiddenIt() {document.getElementById("msg1").style.visibility = "hidden";}setTimeout("hiddenIt()", 15000); function showIt3() {document.getElementById("msg1").style.visibility = "visible";} setTimeout("showIt3()", 25000);  msg1.onclick = function() {document.getElementById('msg1').style.visibility = "hidden"; };function alertW() { document.getElementById("alertWapp").style.visibility = "visible";	} setTimeout("alertW()", 15000); </script>
 	<!-- END Widget WhastApp -->
 	<!-- Chatra {literal} -->
-		<script>
+
+  <script type="text/javascript" src="<?=LAYOUT?>js/jquery-2.2.4.min.js" ></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="<?=LAYOUT?>js/jquery-ui.min.js"></script>  
+	<script>
 		$("#meus_cursos").click(function(e){
 			e.preventDefault();
 			var url_webapp = "<?=URL_BASE?>webapp/integra.php?token=<?=base64_encode($_SESSION['usuario_cpf'])?>";

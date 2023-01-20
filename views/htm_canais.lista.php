8be1277180f92d77343db35310e6fd214ff817a5
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-20 13:20:56 +0000

    Update htm_canais.lista.php

--- views/htm_canais.lista.php
+++ views/htm_canais.lista.php
@@ -721,10 +721,10 @@
 	}
 	
 	// termina topo
-	$url_banner = URL.'/arquivos/backgorund_canal.png';
+	$url_banner = '/arquivos/backgorund_canal.png';
 	?>
 
-<div class="jumbotron banner_canal" style="background-image: url(<?=$url_banner?>);">
+<div class="jumbotron banner_canal" style="background-image: url(<?=URL.$url_banner?>);">
     <div class="container" style="width: 80%;">
         <h1 class="display-3 nome_do_canal">Escolha o seu<br> canal favorito</h1>
     </div>

47c949342c0103a23031e3c8eeeccf4629e7f8f7
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-23 10:55:00 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -16,8 +16,10 @@ class index extends controller {
 		$linha_ = 0;
 		while($linha = fgetcsv($dados,1000,',')){
 			if($linha_ >= 0){
-				$email = $linha[1];
-				$cpf = $linha[2];
+
+				$nome 	= $linha[0];
+				$email 	= $linha[1];
+				$cpf 	= $linha[2];
 
 				$email_lms = $this->check_email_lms($email, $cpf);
 				if($email_lms == 1){
@@ -27,82 +29,68 @@ class index extends controller {
 					$last_id = $this->adiciona_email_lms($email);
 				}
 
-				print_r($last_id);exit;
+				$codigo = substr(time().rand(10000,99999),-15);
+				$tipo = "F";
+				$add_data_gerado = date("Y-m-d H:i:s");
+				$senha_md5       = $this->post('senha');
+				$senha_md5       = md5($senha_md5);
 
-				// $codigo = substr(time().rand(10000,99999),-15);
-				// $tipo = "F";
-				// $add_data_gerado = date("Y-m-d H:i:s");
-				// $senha_md5       = $this->post('senha');
-				// $senha_md5       = md5($senha_md5);
-
-				// $db = new mysql();
-				// $db->inserir("cadastro", array(
-				// 	"fisica_nome"=>"$fisica_nome",
-				// 	"lms_usuario_id"=>"$last_id",
-				// 	"telefone"=>"$telefone",
-				// 	"is_brasil"=>"$country_document",
-				// 	"codigo"=>"$codigo",
-				// 	"tipo"=>"$tipo",
-				// 	"fisica_cpf"=>"$cpf",
-				// 	"email"=>"$email",
-				// 	"cep"=>"$cep",
-				// 	"endereco"=>"$endereco",
-				// 	"numero"=>"$numero",
-				// 	"complemento"=>"$complemento",
-				// 	"bairro"=>"$bairro",
-				// 	"estado"=>"$estado",
-				// 	"cidade"=>"$cidade",
-				// 	"is_brasil_address"=>"$country_document",
-				// 	"etapa"=>2,
-				// 	"senha"=>"$senha_tratada",
-				// 	"senha_md5"=>"$senha_md5",
-				// 	"etapa"=>0,
-				// 	"status" => 0
-				// ));
+				$db = new mysql();
+				$db->inserir("cadastro", array(
+					"fisica_nome"=>"$nome",
+					"lms_usuario_id"=>"$last_id",
+					"codigo"=>"$codigo",
+					"tipo"=>"$tipo",
+					"fisica_cpf"=>"$cpf",
+					"email"=>"$email",
+					"senha_md5"=>"$senha_md5",
+					"etapa"=>0,
+					"status" => 0
+				));
 			
-				// $this->salvar_usuario_lms(  $lms_usuario_id, 
-				// 							$fisica_nome,
-				// 							$email,
-				// 							$fisica_cpf,
-				// 							$telefone,
-				// 							$endereco,
-				// 							$numero,
-				// 							$bairro,
-				// 							$cidade,
-				// 							$estado,$add_data_gerado,
-				// 							$fisica_nascimento,
-				// 							$fisica_sexo,
-				// 							$senha_md5);
+				$this->salvar_usuario_lms(  $last_id, 
+											$nome,
+											$email,
+											null,
+											null,
+											null,
+											null,
+											null,
+											null,
+											null,
+											$add_data_gerado,
+											null,
+											null,
+											$senha_md5);
 
-
-				echo($linha[0]).'<br>';
-				echo($linha[1]).'<br>';
-				echo($linha[2]).'<br>';
-				echo($linha[3]).'<br>';
-				echo($linha[4]).'<br>';
-				echo($linha[5]).'<br>';
-				echo($linha[6]).'<br>';
-				echo($linha[7]).'<br>';
-				echo($linha[8]).'<br>';
-				echo($linha[9]).'<br>';
-				echo($linha[10]).'<br>';
-				echo($linha[11]).'<br>';
-				echo($linha[12]).'<br>';
-				echo($linha[13]).'<br>';
-				echo($linha[14]).'<br>';
-				echo($linha[15]).'<br>';
-				echo($linha[16]).'<br>';
-				echo($linha[17]).'<br>';
-				echo($linha[18]).'<br>';
-				echo($linha[19]).'<br>';
-				echo($linha[20]).'<br>';
-				echo($linha[21]).'<br>';
-				echo($linha[22]).'<br>';
-				echo($linha[23]).'<br>';
-				echo($linha[24]).'<br>';
-				echo($linha[25]).'<br>';
-				echo($linha[26]).'<br>';
-				echo '<hr>';
+				// echo($linha[0]).'<br>';
+				// echo($linha[1]).'<br>';
+				// echo($linha[2]).'<br>';
+				// echo($linha[3]).'<br>';
+				// echo($linha[4]).'<br>';
+				// echo($linha[5]).'<br>';
+				// echo($linha[6]).'<br>';
+				// echo($linha[7]).'<br>';
+				// echo($linha[8]).'<br>';
+				// echo($linha[9]).'<br>';
+				// echo($linha[10]).'<br>';
+				// echo($linha[11]).'<br>';
+				// echo($linha[12]).'<br>';
+				// echo($linha[13]).'<br>';
+				// echo($linha[14]).'<br>';
+				// echo($linha[15]).'<br>';
+				// echo($linha[16]).'<br>';
+				// echo($linha[17]).'<br>';
+				// echo($linha[18]).'<br>';
+				// echo($linha[19]).'<br>';
+				// echo($linha[20]).'<br>';
+				// echo($linha[21]).'<br>';
+				// echo($linha[22]).'<br>';
+				// echo($linha[23]).'<br>';
+				// echo($linha[24]).'<br>';
+				// echo($linha[25]).'<br>';
+				// echo($linha[26]).'<br>';
+				// echo '<hr>';
 			}
 			$linha_++;
 		}

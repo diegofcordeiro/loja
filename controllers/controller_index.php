50c02a5183f1ebe304f1942382b5e6f779fcb9df
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-01-25 11:22:42 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -9584,6 +9584,9 @@ class index extends controller {
 		/////////////////////////////////// SEND TO LMS ///////////////////////////////////
 	}
 	public function criar_json(){
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
 		$array = array(
 			"ada"=> 'a'
 		);

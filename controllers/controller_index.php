c3018ea0c1632d478cc34457f221dcb8c7c66eb3
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-02-22 15:57:50 +0000

    Update controller_index.php

--- controllers/controller_index.php
+++ controllers/controller_index.php
@@ -8594,14 +8594,17 @@ class index extends controller {
 
 		$cod = $_POST['codigo'];
 
-		$email = '2332sdasd@gmail.com';
 
 		$user_exist = $this->check_mercadopago_user($email, $_POST['mercadopago_access_token']);
+
 		if($user_exist == 0){
 			$new_user = $this->add_mercadopago_user($email, $_POST['mercadopago_access_token']);
-			$new_user_id = $new_user->id;
-		}
 
+			$db = new mysql();
+			$db->alterar("cadastr", array(
+				"mercadopago_id"=>"$new_user->id",
+			), " email='$email' ");
+		}
 
 		
 

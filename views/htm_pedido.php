759d24e42bd23cc407aefadbd09a734a88354624
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-02-22 16:12:23 +0000

    mercadopago

--- views/htm_pedido.php
+++ views/htm_pedido.php
@@ -2646,7 +2646,8 @@
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
   	<script type="text/javascript" src="http://code.jquery.com/qunit/qunit-1.11.0.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
+	<script src="https://sdk.mercadopago.com/js/v2"></script>
+  	<script type="text/javascript" src="index_mercadopago.js" defer></script>
 <script type="text/javascript">
 	
 	

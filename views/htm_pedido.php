454337eac6de7b263d23c478ad349c7ccd66a42a
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-02-22 17:22:41 +0000

    Update htm_pedido.php

--- views/htm_pedido.php
+++ views/htm_pedido.php
@@ -2708,11 +2708,121 @@
   	<script type="text/javascript" src="http://code.jquery.com/qunit/qunit-1.11.0.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>
-  	<script type="text/javascript" src="index_mercadopago.js" defer></script>
+
 <script type="text/javascript">
-	
-	
+console.log('aqui');
+window.Mercadopago.setPublishableKey("TEST-e977e890-5e81-4c9f-b62c-4a5046eb9c4b");
+console.log('aqui');
+    $(function(){
+        window.Mercadopago.getIdentificationTypes();
+
+        document.getElementById('cardNumber_mp').addEventListener('change', guessPaymentMethod);
+
+        function guessPaymentMethod(event) {
+            console.log('aqui');
+            let cardnumber_mp = document.getElementById("cardNumber_mp").value;
+            if (cardnumber_mp.length >= 6) {
+                let bin = cardnumber_mp.substring(0,6);
+                window.Mercadopago.getPaymentMethod({
+                    "bin": bin
+                }, setPaymentMethod);
+            }
+        };
+
+        function setPaymentMethod(status, response) {
+            if (status == 200) {
+                let paymentMethod = response[0];
+                document.getElementById('paymentMethodId').value = paymentMethod.id;
+
+                getIssuers(paymentMethod.id);
+            } else {
+                alert(`payment method info error: ${response}`);
+            }
+        }
+
+
+        function getIssuers(paymentMethodId) {
+            window.Mercadopago.getIssuers(
+                paymentMethodId,
+                setIssuers
+            );
+        }
+
+        function setIssuers(status, response) {
+            if (status == 200) {
+                let issuerSelect = document.getElementById('issuer');
+                response.forEach( issuer => {
+                    let opt = document.createElement('option');
+                    opt.text = issuer.name;
+                    opt.value = issuer.id;
+                    issuerSelect.appendChild(opt);
+                });
+
+                getInstallments(
+                    document.getElementById('paymentMethodId').value,
+                    document.getElementById('transactionAmount').value,
+                    issuerSelect.value
+                );
+            } else {
+                alert(`issuers method info error: ${response}`);
+            }
+        }
+
+        function getInstallments(paymentMethodId, transactionAmount, issuerId){
+                window.Mercadopago.getInstallments({
+                    "payment_method_id": paymentMethodId,
+                    "amount": parseFloat(transactionAmount),
+                    "issuer_id": parseInt(issuerId)
+                }, setInstallments);
+        }
+
+        function setInstallments(status, response){
+            if (status == 200) {
+                document.getElementById('installments').options.length = 0;
+                response[0].payer_costs.forEach( payerCost => {
+                    let opt = document.createElement('option');
+                    opt.text = payerCost.recommended_message;
+                    opt.value = payerCost.installments;
+                    document.getElementById('installments').appendChild(opt);
+                });
+            } else {
+                alert(`installments method info error: ${response}`);
+            }
+        }
+
+
+        doSubmit = false;
+        document.getElementById('paymentForm').addEventListener('submit', getCardToken);
+
+        function getCardToken(event){
+            event.preventDefault();
+            if(!doSubmit){
+                let $form = document.getElementById('paymentForm');
+                window.Mercadopago.createToken($form, setCardTokenAndPay);
+                return false;
+            }
+        };
+
+        function setCardTokenAndPay(status, response) {
+            if (status == 200 || status == 201) {
+                let form = document.getElementById('paymentForm');
+                let card = document.createElement('input');
+                card.setAttribute('name', 'token');
+                card.setAttribute('type', 'hidden');
+                card.setAttribute('value', response.id);
+                form.appendChild(card);
+                doSubmit=true;
+                form.submit();
+            } else {
+                alert("Verify filled data!\n"+JSON.stringify(response, null, 4));
+            }
+        };
+
+
+    });
+
 </script>
+
 <script type="text/javascript">
 /* Máscaras ER */
 function mascara(o,f){

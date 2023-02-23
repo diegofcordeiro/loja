6d51d0070cc2926cfe35cadef8abb603075abde2
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-02-23 11:37:22 +0000

    Update htm_pedido.php

--- views/htm_pedido.php
+++ views/htm_pedido.php
@@ -1131,6 +1131,105 @@
 						<div style="margin-top:20px;" >
 							<?php //echo '<pre>'; print_r($email);exit; ?>
 
+							<style>
+								*{
+									padding: 0;
+									margin: 0;
+									border: 0;
+								}
+								
+								#form-checkout {
+								display: flex;
+								flex-direction: column;
+								width: 60%;
+								margin: 100px 20%;
+								padding: 10px 20px;
+								background-color: #eee;	
+								}
+
+								.container {
+								height: auto;
+								display: block;
+								margin-top: 10px; 
+								}
+								
+								.container iframe{
+									background-color: #fff !important;
+									padding: 10px 20px !important;
+									width: 95.6%;
+									height: 18px !important;
+								}
+								
+								h1{
+									font-size: 1.6em;
+									font-weight: 600;
+									font-family: 'Montserrat', sans-serif;
+									text-transform: uppercase;
+									margin-top: 30px;
+								}
+								
+								p{
+									font-size: 1em;
+									font-weight: 400;
+									font-family: 'Montserrat', sans-serif;
+									text-align: center;
+									margin: 10px 0;
+								}
+								
+								label{
+									margin-top: 10px;
+									margin-bottom: 3px;
+									font-size: 1em;
+									font-weight: 400;
+									font-family: 'Montserrat', sans-serif;
+									display: block;
+								}
+								
+								input[type=text], input[type=email], input[type=number]{
+									padding: 10px 20px;
+									margin-top: 10px;
+									margin-bottom: 3px;
+									font-size: 1em;
+									font-weight: 400;
+									font-family: 'Montserrat', sans-serif;
+									display: block;
+									background-color: #fff;
+									width: 100%;
+								}
+								
+								select{
+									padding: 10px 20px;
+									margin-top: 10px;
+									margin-bottom: 3px;
+									font-size: 1em;
+									font-weight: 400;
+									font-family: 'Montserrat', sans-serif;
+									display: block;
+									background-color: #fff;
+									width: 100%;
+								}
+								
+								button{
+									margin-top: 10px;
+									padding: 10px 20px;
+									font-size: 1em;
+									font-weight: 400;
+									font-family: 'Montserrat', sans-serif;
+									width: 100%;
+									background-color: #662747;
+									color: #fff;
+									border-radius: 5px;
+									-moz-border-radius: 5px;
+									-webkit-border-radius: 5px;
+									cursor: pointer;
+								}
+								
+								button:hover{
+									background-color: #511f38;
+								}
+								
+							</style>
+
 							<form id="form-checkout" action="<?=DOMINIO?>index/mercadopago_flow" method="POST">
 								<p><b>Nome:</b> APRO - 
 								<b>Cartão:</b> 5031433215406351 -

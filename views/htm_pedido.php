a2ee6d85f7379e2a25eb2a5621eac1367dad4456
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-02-23 18:39:19 +0000

    Update htm_pedido.php

--- views/htm_pedido.php
+++ views/htm_pedido.php
@@ -1188,6 +1188,7 @@
 								<input type="hidden"  name="mercadopago_client_secret" value="<?=$forma_pagamento->mercadopago_client_secret?>">
 								<input type="hidden"  name="mercadopago_public_key" value="<?=$forma_pagamento->mercadopago_public_key?>">
 								<input type="hidden"  name="mercadopago_access_token" value="<?=$forma_pagamento->mercadopago_access_token?>">
+								<input type="text" name="codigo" value="<?=$data_pedido->codigo?>">
 
 								<button type="submit" id="form-checkout__submit">Pagar Agora</button>
 							</form>

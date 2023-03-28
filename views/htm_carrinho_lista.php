8aa3387ff0a545aeb30d4180cee9bcdd9a66eea7
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2023-03-28 16:08:57 +0100

    Update htm_carrinho_lista.php

--- views/htm_carrinho_lista.php
+++ views/htm_carrinho_lista.php
@@ -1,15 +1,18 @@
-<?php if(!isset($_base['libera_views'])){ header("HTTP/1.0 404 Not Found"); exit; } ?>
+<?php if (!isset($_base['libera_views'])) {
+	header("HTTP/1.0 404 Not Found");
+	exit;
+} ?>
 
-<table class="table tabela_boa" >
+<table class="table tabela_boa">
 
 	<thead>
 		<tr>
 			<th style='width:30px;'></th>
 			<th>Produto</th>
 			<th></th>
-			<th style='text-align:center;' >Preço</th>
+			<th style='text-align:center;'>Preço</th>
 			<!-- <th style='text-align:center; width:50px;' >Quantidade</th> -->
-			<th style='text-align:center; width:120px;' >Total</th>
+			<th style='text-align:center; width:120px;'>Total</th>
 
 		</tr>
 	</thead>
@@ -30,32 +33,31 @@
 
 	$n = 0;
 	$total_pedidos_ = 0;
-	foreach ($carrinho['lista'] as $key => $values) { 
+	foreach ($carrinho['lista'] as $key => $values) {
 		// echo '<pre>';print_r(($values));exit;
 		$subtotal_ = 0;
-		
-		foreach ($values as $key2 => $value) { 
 
-			if($value['usar_valor_vindi'] == 1){
+		foreach ($values as $key2 => $value) {
+
+			if ($value['usar_valor_vindi'] == 1) {
 				$valor_unitario = '-';
 				$total_geral = '-';
 				$subtotal_ = $value['combo_valor'];
-				
-			}else{
-				$valor_unitario = "R$ ".$value['total_unitario'];
-				$total_geral = "R$ ".$value['total_quantidade'];
+			} else {
+				$valor_unitario = "R$ " . $value['total_unitario'];
+				$total_geral = "R$ " . $value['total_quantidade'];
 
 				$resultado = str_replace('.', '', $value['total_unitario']); // remove o ponto
-    			$resultado = str_replace(',', '.', $resultado); // substitui a vírgula por ponto
+				$resultado = str_replace(',', '.', $resultado); // substitui a vírgula por ponto
 				$subtotal_ = ($subtotal_ + floatval($resultado));
 			}
 			// echo '<pre>';print_r(count($values));exit;
-			if($key != 0){
-				if($key2 == 0){
+			if ($key != 0) {
+				if ($key2 == 0) {
 					echo "
 						<tr>
 							<td colspan='5' style='border-bottom: 1px white solid;'>
-								<div class='carrinho_lista_remover' style='padding-top:0px !important'><a href='#' onClick=\"remove_carrinho('".$value['id']."');\" ><i class='fa fa-times' aria-hidden='true'></i></a></div>".$value['combo_titulo']."
+								<div class='carrinho_lista_remover' style='padding-top:0px !important'><a href='#' onClick=\"remove_carrinho('" . $value['id'] . "');\" ><i class='fa fa-times' aria-hidden='true'></i></a></div>" . $value['combo_titulo'] . "
 							</td>
 						</tr>
 					";
@@ -68,34 +70,34 @@
 					</td>
 
 					<td colspan='2' >
-					<div class='carrinho_lista_imagem' style='background-image:url(".$value['imagem'].");' ></div>
-					<div class='carrinho_lista_texto' ><div style='padding-left:15px; padding-right:15px;'>".$value['titulo']."</div></div>
+					<div class='carrinho_lista_imagem' style='background-image:url(" . $value['imagem'] . ");' ></div>
+					<div class='carrinho_lista_texto' ><div style='padding-left:15px; padding-right:15px;'>" . $value['titulo'] . "</div></div>
 					</td>
 
 					<td style='text-align:center;' >
-					<div class='carrinho_lista_valor' >".$valor_unitario."</div>
+					<div class='carrinho_lista_valor' >" . $valor_unitario . "</div>
 					</td>";
-					// <td style='width:200px;' ><div style='margin-top:30px; text-align:center;'>
-					// <input class='carrinho_quantidade_input' name='quantidade_".$value['id']."' id='quantidade_".$value['id']."' value='".$value['quantidade']."' onkeypress='Mascara(this,Integer)' onKeyDown='Mascara(this,Integer)' >
-					// <button type='button' class='botao_padrao ".$botao_css."' onClick=\"altera_quantidade('".$value['id']."')\" >Alterar</button>	</div>	 
-					
-					echo "</td>
+				// <td style='width:200px;' ><div style='margin-top:30px; text-align:center;'>
+				// <input class='carrinho_quantidade_input' name='quantidade_".$value['id']."' id='quantidade_".$value['id']."' value='".$value['quantidade']."' onkeypress='Mascara(this,Integer)' onKeyDown='Mascara(this,Integer)' >
+				// <button type='button' class='botao_padrao ".$botao_css."' onClick=\"altera_quantidade('".$value['id']."')\" >Alterar</button>	</div>	 
+
+				echo "</td>
 
 					<td style='text-align:center; width:120px;' >
-					<div class='carrinho_lista_valor' >".$total_geral."</div>
+					<div class='carrinho_lista_valor' >" . $total_geral . "</div>
 					</td>
 
 					</tr>
 				";
-				if($key2 == count($values)-1){
+				if ($key2 == count($values) - 1) {
 					echo "
 						<tr>
 							<td colspan='6' style='padding: 1px !important;background: #dddddd;'></td>
 						</tr>
 					";
 				}
-			}else{
-				if($key2 == 0){
+			} else {
+				if ($key2 == 0) {
 					echo "
 						<tr>
 							<td colspan='5' style='border-bottom: 1px white solid;'>
@@ -108,27 +110,27 @@
 					<tr>
 
 					<td>
-					<div class='carrinho_lista_remover' ><a href='#' onClick=\"remove_carrinho('".$value['id']."');\" ><i class='fa fa-times' aria-hidden='true'></i></a></div>
+					<div class='carrinho_lista_remover' ><a href='#' onClick=\"remove_carrinho('" . $value['id'] . "');\" ><i class='fa fa-times' aria-hidden='true'></i></a></div>
 					</td>
 
 					<td colspan='2' >
-					<div class='carrinho_lista_imagem' style='background-image:url(".$value['imagem'].");' ></div>
-					<div class='carrinho_lista_texto' ><div style='padding-left:15px; padding-right:15px;'>".$value['titulo'].$value['id_combo']."</div></div>
+					<div class='carrinho_lista_imagem' style='background-image:url(" . $value['imagem'] . ");' ></div>
+					<div class='carrinho_lista_texto' ><div style='padding-left:15px; padding-right:15px;'>" . $value['titulo'] . $value['id_combo'] . "</div></div>
 					</td>
 
 					<td style='text-align:center;' >
-					<div class='carrinho_lista_valor'>".$valor_unitario."</div>
+					<div class='carrinho_lista_valor'>" . $valor_unitario . "</div>
 					</td>";
 
-					// <td style='width:200px;' >
-					// <div style='margin-top:30px; text-align:center;'>
-					// <input class='carrinho_quantidade_input' name='quantidade_".$value['id']."' id='quantidade_".$value['id']."' value='".$value['quantidade']."' onkeypress='Mascara(this,Integer)' onKeyDown='Mascara(this,Integer)' >
-					// <button type='button' class='botao_padrao ".$botao_css."' onClick=\"altera_quantidade('".$value['id']."')\" ><i style='font-size:15px' class='fa'>&#xf021;</i></button>		 
-					// </div>
-					// </td>
+				// <td style='width:200px;' >
+				// <div style='margin-top:30px; text-align:center;'>
+				// <input class='carrinho_quantidade_input' name='quantidade_".$value['id']."' id='quantidade_".$value['id']."' value='".$value['quantidade']."' onkeypress='Mascara(this,Integer)' onKeyDown='Mascara(this,Integer)' >
+				// <button type='button' class='botao_padrao ".$botao_css."' onClick=\"altera_quantidade('".$value['id']."')\" ><i style='font-size:15px' class='fa'>&#xf021;</i></button>		 
+				// </div>
+				// </td>
 
-					echo "<td style='text-align:center; width:120px;' >
-					<div class='carrinho_lista_valor' >".$total_geral."</div>
+				echo "<td style='text-align:center; width:120px;' >
+					<div class='carrinho_lista_valor' >" . $total_geral . "</div>
 					</td>
 
 					</tr>
@@ -140,15 +142,15 @@
 
 		echo "
 			<tr>
-			<td colspan='4' style='text-align:right; ' >Sub-total</td>
-			<td style='text-align:center;  width:120px; font-weight:bold;' >R$ ".number_format($subtotal_,2)."</td> 
+			<td colspan='4' style='text-align:right; font-size: 18px;' >Sub-total</td>
+			<td style='text-align:center;  width:120px; font-weight:bold;font-size: 18px;' >R$ " . number_format($subtotal_, 2) . "</td> 
 			</tr>
 		";
 		$total_pedidos_ = ($total_pedidos_ + $subtotal_);
 	}
 
 
-	if($n != 0){
+	if ($n != 0) {
 
 		// echo "
 		// <tr>
@@ -164,17 +166,16 @@
 		<td colspan='5' style='text-align:center; padding-top:120px; padding-bottom:120px; font-size:20px;' >Seu carrinho está vazio.</td>
 		</tr>
 		";
-
 	}
 
 	?>
 
 </table>
 <table class="table tabela_boa">
-	
+
 	<tr>
-		<td style='text-align:right; border-top:0px;' >Total do Pedido</td>
-		<td style='text-align:center; width:120px; font-weight:bold; border-top:0px;' >R$ <?=number_format($total_pedidos_,2)?></td>
-	</tr> 
+		<td style='text-align:right; border-top:0px;font-size: 18px;'>Total do Pedido</td>
+		<td style='text-align:center; width:120px; font-weight:bold; border-top:0px;font-size: 18px;'>R$ <?= number_format($total_pedidos_, 2) ?></td>
+	</tr>
 
 </table>
